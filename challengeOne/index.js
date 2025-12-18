10cff3a261a8c0edbd71c90a82af809ca4d87e19
Karson Lant <lantk991@gmail.com>
2025-12-18 14:26:03 -0800

    getEvens and object testing

--- challengeOne/index.js
+++ challengeOne/index.js
@@ -1,5 +1,14 @@
-function OutputNum() {
-    console.log(42)
+const users = [
+  { name: "Alex", age: 20 },
+  { name: "Sam", age: 30 },
+  { name: "Jordan", age: 25 }
+];
+
+const nums = [1, 2, 3, 4, 5]
+
+function getEvens(arr) {
+    const newArr = arr.map((num) => num * num)
+    return newArr
 }
 
-OutputNum()
+console.log(getEvens(nums))
